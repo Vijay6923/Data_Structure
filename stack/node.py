120588086da8ad5e19cf2b61551e776954154ff0
Vijay6923 <kumarviju2468@gmail.com>
2024-06-03 12:53:18 +0530

    data stucture

---
+++ stack/node.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self,data,next=None):
+        self.data=data
+        self.next=next
+class Stack:
+    def __init__(self):
+        self.__size=0
+        self.__head=None
+
+def size(self):
+    return  self.__size
+def isEmpty(self):
+    return (self.size()==0)
+def push(self,val):
+    newNode=Node(val,head)
+    self.head=newNode
+    
+
+
+
+
+def pop(self):
+    if self.isEmpty():
+        raise Exception("stack overflow")
+    data=self.__head.__data
+    temp=self.__head
+    self.head=self.head.node
+
+
+
+def top(self):
+
+        
+    
